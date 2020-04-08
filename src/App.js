696191af3685ab10dbaac1acda30d8477b52b69b
yasir <aff3ad@gmail.com>
2020-04-08 22:05:00 +0300

    5.7* Blog list frontend, step7

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ const App = () => {
   const [errMsg, setErrMsg] = useState('')
   const [showMsg, setMsg] = useState('')
   const [shouldShow, setShouldShow] = useState(true)
+
+
   
 
   useEffect(()=>{
@@ -78,7 +80,7 @@ const showBlogs = () => {
         <p>{user.name} logged in <button onClick={handleLogout}> logout </button></p>
         {isShowBlog} 
         <h2>blogs</h2>
-        {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
+        {blogs.map(blog => <Blog key={blog.id} blog={blog}/>)}
       </div>
     )
   }
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,40 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, {useState} from 'react'
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 2,
+    marginBottom: 5
+  }
+  const [viewBlog, setViewBlog] = useState(false)
+  const blogDetailView = () => {
+    return(
+      <div>
+        {blog.title} <button onClick={()=>setViewBlog(false)}>hide</button>
+        <br/> 
+        {blog.url}
+        <br/>
+        likes {blog.likes} <button>likes</button>
+        <br/>
+        {blog.author}
+      </div>
+    )
+  }
 
+  const blogNormalView = () => {
+    return(
+      <div>
+        {blog.title} {blog.author} <button onClick={()=>setViewBlog(true)}> view </button>
+      </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+       {viewBlog ? blogDetailView(): blogNormalView()}
+  </div>
+  )
+
+}
 export default Blog
