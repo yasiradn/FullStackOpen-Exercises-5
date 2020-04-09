5da55eaca65a607aad6a476f917dbec76719974c
yasir <aff3ad@gmail.com>
2020-04-09 13:09:04 +0300

    5.11: Blog list frontend, step11

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import PropTypes from 'prop-types'
 const LoginForm = ({handleLogin, handleUsernameChange, handlePasswordChange, username, password}) => {
     return(
       <div>
@@ -15,6 +15,14 @@ const LoginForm = ({handleLogin, handleUsernameChange, handlePasswordChange, use
      </form>
     </div>
     )
+}
+
+LoginForm.prototype = {
+  handleLogin: PropTypes.func.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired
 }
 
 export default LoginForm
